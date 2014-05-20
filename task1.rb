3303821559470f3c4466f6288be9b9a7c521c8fb
elenadm <elena.an.dmitrieva@gmail.com>
2014-05-20 23:15:51 +0300

    Implementation of sum(array)

--- task1.rb
+++ task1.rb
@@ -2,7 +2,9 @@
 # and returns the sum of its elements.
 # For an empty array it should return zero.
 def sum(array)
-  raise "Not yet implemented"
+  res = []
+  array.each { |a| res += [a] }
+  res
 end
 
 # Takes an array of integers as an argument
